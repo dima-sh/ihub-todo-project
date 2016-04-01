544de8b07f0485fd52c9b2da8a7e77a5c4a9e81a
Dima Sh <shimdim@gmail.com>
2016-04-02 02:15:50 +0300

    bar layout

--- src/client/bar.js
+++ src/client/bar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 class TopBar extends React.Component {
+
+
     render() {
 
         return (
--- src/client/project.js
+++ src/client/project.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header';
+import TopBar from './bar';
 
 const Project = ({ name, id }) => (
 
   <article>
     <Header name={name} projectId={id} />
+    <TopBar />
   </article>
 );
 
